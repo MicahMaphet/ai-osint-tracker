454dd90b522e8df1e55e78d20fb62b4fd0f7ea2b
MicahMaphet <micahmaphet@gmail.com>
2025-06-27 03:27:20 +0900

    wrote ScanMovement script in c++ for performance

---
+++ ScanMovement.cpp
@@ -0,0 +1,49 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** args) {
+    VideoCapture vidReader("rawusing.mp4");
+
+    // Check if video is imported successfully
+    if (vidReader.isOpened() == false) {
+        cout << "Cannot open the video file" << endl;
+        cin.get(); //wait for any key press
+        return -1;
+    }
+
+    int frames = (int)vidReader.get(CAP_PROP_FRAME_COUNT);
+    int fps = (int)vidReader.get(CAP_PROP_FPS);
+
+    double dWidth = vidReader.get(CAP_PROP_FRAME_WIDTH);
+    double dHeight = vidReader.get(CAP_PROP_FRAME_HEIGHT);
+    Size frameSize(dWidth, dHeight);
+
+    VideoWriter vidWriter("output.mp4", VideoWriter::fourcc('m', 'p', '4', 'v'), fps, frameSize, true);
+
+
+    Mat prevFrame;
+    Mat frame;
+        
+    vidReader.read(prevFrame);
+    for (int i=0; i<frames; i++) {
+        vidReader.read(frame);
+        for (int y=0; y<frame.rows;y++) {
+            for (int x=0; x<frame.cols;x++) {
+                Vec3b& pixel = frame.at<Vec3b>(y, x);
+                Vec3b& prevPixel = prevFrame.at<Vec3b>(y, x);
+                if (abs(pixel[0] + pixel[1] + pixel[2] - (prevPixel[0] + prevPixel[1] + prevPixel[2])) < 1) {
+                    pixel[0] = 0;
+                    pixel[1] = 0;
+                    pixel[2] = 0;
+                }
+
+            }
+        }
+        vidWriter.write(frame);
+    }
+    cout << "Successfully scanned video for pixel changes (at output.mp4)" << endl;
+    return 0;
+}
